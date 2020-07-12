65b33b673a1c36b108f2a1ea8de34be0f2ac2916
CaddyDz <caddydz4@gmail.com>
2020-07-12 15:58:07 +0100

    Added static model property
    So morphMany fields would work
    
    Signed-off-by: CaddyDz <caddydz4@gmail.com>

--- src/Pages/StaticResource.php
+++ src/Pages/StaticResource.php
@@ -10,215 +10,221 @@ use Laravel\Nova\Http\Requests\NovaRequest;
 
 abstract class StaticResource extends Resource
 {
-    use Concerns\ResolvesResourceFields;
+	use Concerns\ResolvesResourceFields;
 
-    /**
-     * The single value that should be used to represent the resource when being displayed.
-     *
-     * @var string
-     */
-    public static $title = 'title';
+	/**
+	 * The model the resource corresponds to.
+	 *
+	 * @var string
+	 */
+	public static $model;
 
-    /**
-     * Indicates if the resoruce should be globally searchable.
-     * Disabled for now until there is a fix for issue #15
-     *
-     * @var bool
-     */
-    public static $globallySearchable = false;
+	/**
+	 * The single value that should be used to represent the resource when being displayed.
+	 *
+	 * @var string
+	 */
+	public static $title = 'title';
 
-    /**
-     * The columns that should be searched.
-     *
-     * @var array
-     */
-    public static $search = [
-        'title', 'name'
-    ];
+	/**
+	 * Indicates if the resoruce should be globally searchable.
+	 * Disabled for now until there is a fix for issue #15
+	 *
+	 * @var bool
+	 */
+	public static $globallySearchable = false;
 
-    /**
-     * Indicates if the resource should be displayed in the sidebar.
-     *
-     * @var bool
-     */
-    public static $displayInNavigation = false;
+	/**
+	 * The columns that should be searched.
+	 *
+	 * @var array
+	 */
+	public static $search = [
+		'title', 'name'
+	];
 
-    /**
-     * Create a new resource instance.
-     *
-     * @param \Whitecube\NovaPage\Pages\Template $resource
-     * @return void
-     */
-    public function __construct(Template $resource)
-    {
-        parent::__construct($resource);
-    }
+	/**
+	 * Indicates if the resource should be displayed in the sidebar.
+	 *
+	 * @var bool
+	 */
+	public static $displayInNavigation = false;
 
-    /**
-     * Determine if this resource uses soft deletes.
-     *
-     * @return bool
-     */
-    public static function softDeletes()
-    {
-        return false;
-    }
+	/**
+	 * Create a new resource instance.
+	 *
+	 * @param \Whitecube\NovaPage\Pages\Template $resource
+	 * @return void
+	 */
+	public function __construct(Template $resource)
+	{
+		parent::__construct($resource);
+	}
 
-    /**
-     * Get the search result subtitle for the resource.
-     *
-     * @return string
-     */
-    public function subtitle()
-    {
-        return $this->resource->getName();
-    }
+	/**
+	 * Determine if this resource uses soft deletes.
+	 *
+	 * @return bool
+	 */
+	public static function softDeletes()
+	{
+		return false;
+	}
 
-    /**
-     * Get a fresh instance of the model represented by the resource.
-     *
-     * @return mixed
-     */
-    public static function newModel()
-    {
-        if(request()->resourceId) {
-            return resolve(Manager::class)
-                ->newQueryWithoutScopes()
-                ->whereKey(request()->resourceId)
-                ->firstOrFail();
-        }
-        return resolve(Manager::class);
-    }
+	/**
+	 * Get the search result subtitle for the resource.
+	 *
+	 * @return string
+	 */
+	public function subtitle()
+	{
+		return $this->resource->getName();
+	}
 
-    /**
-     * Get the fields displayed by the resource.
-     *
-     * @param  \Illuminate\Http\Request $request
-     * @return array
-     */
-    public function fields(Request $request)
-    {
-        return array_merge(
-            $this->getFormIntroductionFields(),
-            $this->getTemplateAttributesFields($request)
-        );
-    }
+	/**
+	 * Get a fresh instance of the model represented by the resource.
+	 *
+	 * @return mixed
+	 */
+	public static function newModel()
+	{
+		if (request()->resourceId) {
+			return resolve(Manager::class)
+				->newQueryWithoutScopes()
+				->whereKey(request()->resourceId)
+				->firstOrFail();
+		}
+		return resolve(Manager::class);
+	}
 
-    /**
-     * Get the base fields displayed at the top of the resource's form.
-     *
-     * @return array
-     */
-    abstract protected function getFormIntroductionFields();
+	/**
+	 * Get the fields displayed by the resource.
+	 *
+	 * @param  \Illuminate\Http\Request $request
+	 * @return array
+	 */
+	public function fields(Request $request)
+	{
+		return array_merge(
+			$this->getFormIntroductionFields(),
+			$this->getTemplateAttributesFields($request)
+		);
+	}
 
-    /**
-     * Get the base attributes Nova Panel
-     *
-     * @return array
-     */
-    abstract protected function getIndexTableFields();
+	/**
+	 * Get the base fields displayed at the top of the resource's form.
+	 *
+	 * @return array
+	 */
+	abstract protected function getFormIntroductionFields();
 
-    /**
-     * Get the fields displayed by the template.
-     *
-     * @param  \Illuminate\Http\Request $request
-     * @return array
-     */
-    protected function getTemplateAttributesFields(Request $request)
-    {
-        return $this->resource->fields($request);
-    }
+	/**
+	 * Get the base attributes Nova Panel
+	 *
+	 * @return array
+	 */
+	abstract protected function getIndexTableFields();
 
-    /**
-     * Get the cards available for the request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array
-     */
-    public function cards(Request $request)
-    {
-        return $this->resource->cards($request);
-    }
+	/**
+	 * Get the fields displayed by the template.
+	 *
+	 * @param  \Illuminate\Http\Request $request
+	 * @return array
+	 */
+	protected function getTemplateAttributesFields(Request $request)
+	{
+		return $this->resource->fields($request);
+	}
 
-    /**
-     * Get the filters available for the resource.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array
-     */
-    public function filters(Request $request)
-    {
-        return [];
-    }
+	/**
+	 * Get the cards available for the request.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return array
+	 */
+	public function cards(Request $request)
+	{
+		return $this->resource->cards($request);
+	}
 
-    /**
-     * Get the lenses available for the resource.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array
-     */
-    public function lenses(Request $request)
-    {
-        return [];
-    }
+	/**
+	 * Get the filters available for the resource.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return array
+	 */
+	public function filters(Request $request)
+	{
+		return [];
+	}
 
-    /**
-     * Get the actions available for the resource.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array
-     */
-    public function actions(Request $request)
-    {
-        return [];
-    }
+	/**
+	 * Get the lenses available for the resource.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return array
+	 */
+	public function lenses(Request $request)
+	{
+		return [];
+	}
 
-    /**
-     * Determine if the current user can create new resources.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return bool
-     */
-    public static function authorizedToCreate(Request $request)
-    {
-        return false;
-    }
+	/**
+	 * Get the actions available for the resource.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return array
+	 */
+	public function actions(Request $request)
+	{
+		return [];
+	}
 
-    /**
-     * Determine if the current user can delete the given resource.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return bool
-     */
-    public function authorizedToDelete(Request $request)
-    {
-        return false;
-    }
+	/**
+	 * Determine if the current user can create new resources.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return bool
+	 */
+	public static function authorizedToCreate(Request $request)
+	{
+		return false;
+	}
 
-    /**
-     * Prepare the resource for JSON serialization.
-     *
-     * @return array
-     */
-    public function jsonSerialize()
-    {
-        return $this->serializeWithId($this->resolveFields(resolve(NovaRequest::class)));
-    }
+	/**
+	 * Determine if the current user can delete the given resource.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return bool
+	 */
+	public function authorizedToDelete(Request $request)
+	{
+		return false;
+	}
 
-    /**
-     * Prepare the resource for JSON serialization using the given fields.
-     *
-     * @param  \Illuminate\Support\Collection  $fields
-     * @return array
-     */
-    protected function serializeWithId(Collection $fields)
-    {
-        return [
-            'id' => tap(ID::make('id', function() {
-                        return $this->getKey();
-                    }))->resolve($this->resource),
-            'fields' => $fields->all(),
-        ];
-    }
+	/**
+	 * Prepare the resource for JSON serialization.
+	 *
+	 * @return array
+	 */
+	public function jsonSerialize()
+	{
+		return $this->serializeWithId($this->resolveFields(resolve(NovaRequest::class)));
+	}
 
+	/**
+	 * Prepare the resource for JSON serialization using the given fields.
+	 *
+	 * @param  \Illuminate\Support\Collection  $fields
+	 * @return array
+	 */
+	protected function serializeWithId(Collection $fields)
+	{
+		return [
+			'id' => tap(ID::make('id', function () {
+				return $this->getKey();
+			}))->resolve($this->resource),
+			'fields' => $fields->all(),
+		];
+	}
 }
